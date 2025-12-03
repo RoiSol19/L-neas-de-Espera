a66a0c3247f3a923426bafb3cac858ebd6192ee5
RoiSol19 <roy.gonzr20@gmail.com>
2025-12-02 20:32:44 -0600

    Crear Menu

---
+++ LineasDeEspera.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ArbolesBinarios {
+    public static void main(String[] args) {
+        Scanner sol = new Scanner (System.in);
+        int opcion;
+        do{
+            System.out.println("\n \n \tMenu");
+            System.out.println();
+
+        }
+        while(opcion!=3)
+    }
+}
