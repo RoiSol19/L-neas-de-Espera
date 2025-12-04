e1da7b1fd279a5643afc616ac1490bf58dce81b5
RoiSol19 <roy.gonzr20@gmail.com>
2025-12-03 18:14:09 -0600

    Utilización del sistema (P)

--- LineasDeEspera.java
+++ LineasDeEspera.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 
-public class ArbolesBinarios {
+public class LineasDeEspera {
     public static void main(String[] args) {
         Scanner sol = new Scanner (System.in);
         int opcion;
         do{
             System.out.println("\n \n \tMenu");
-            System.out.println();
+            System.out.println("1. M/M/1");
+            System.out.println("2. M/M/1/K");
+            System.out.println("Opcion: ");
+            opcion = sol.nextInt();
+            switch(opcion){
+                case 1->{
+                    System.out.println("Ingresa el promedio de llegadas (λ): ");
+                    int llegada = sol.nextInt();
+                    System.out.println("Ingresa el promedio de servicios (μ): ");
+                    int servicio = sol.nextInt();
+                    int P=llegada/servicio;
 
+                }
+            }
         }
-        while(opcion!=3)
+        while(opcion!=3);
     }
 }
