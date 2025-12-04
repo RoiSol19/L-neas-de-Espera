cbd3889cc853d71f4bfcf944b36a7416c032483e
RoiSol19 <roy.gonzr20@gmail.com>
2025-12-03 20:40:17 -0600

    M/M/1 Finalizado empezar M/M/C

--- LineasDeEspera.java
+++ LineasDeEspera.java
@@ -7,7 +7,7 @@ public class LineasDeEspera {
         do{
             System.out.println("\n \n \tMenu");
             System.out.println("1. M/M/1");
-            System.out.println("2. M/M/1/K");
+            System.out.println("2. M/M/C");
             System.out.println("Opcion: ");
             opcion = sol.nextInt();
             switch(opcion){
@@ -16,8 +16,25 @@ public class LineasDeEspera {
                     int llegada = sol.nextInt();
                     System.out.println("Ingresa el promedio de servicios (μ): ");
                     int servicio = sol.nextInt();
-                    int P=llegada/servicio;
-
+                    double P=llegada/servicio;
+                    double Lq=(Math.pow(llegada,2))/(servicio*(servicio-llegada));
+                    double wq=Lq/llegada;
+                    double w=wq+(1/servicio);
+                    System.out.println("Utilización del sistema (P): "+P);
+                    System.out.println("Número promedio de clientes en la cola (Lq): "+ Lq);
+                    System.out.println("Tiempo promedio de espera en la cola (Wq): "+ wq);
+                    System.out.println("Tiempo total en el sistema (W): "+ w);
+                }
+                case 2->{
+                    System.out.println("Ingresa el promedio de llegadas (λ): ");
+                    double llegada = sol.nextDouble();
+                    System.out.println("Ingresa el promedio de servicios (μ): ");
+                    double servicio = sol.nextDouble();
+                    System.out.println("Ingresa el tamaño del servidor (c): ");
+                    int c= sol.nextInt();
+                    double a=llegada/servicio;
+                    double p=a/c;
+                    
                 }
             }
         }
