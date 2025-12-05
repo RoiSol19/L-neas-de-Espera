337e3cc633ba2c7847b27a265117bae62afef75c
RoiSol19 <roy.gonzr20@gmail.com>
2025-12-05 07:51:01 -0600

    Pedirle al usuario los datos a travez de recursividad

--- MaxYMin.java
+++ MaxYMin.java
@@ -11,10 +11,33 @@ public class MaxYMin {
             System.out.println("Opcion: ");
             switch (opcion) {
                 case 1->{
-                    
+                    simplex(sol, true);
                 }
             }
         }while (opcion!=3);
     }
     
+}
+public static void simplex (Scanner sol, boolean maximizar){
+    System.out.println("Ingresa el coeficiente de x1 en  una funcion objetivo: ");
+    double x1=sol.nextDouble();
+    System.out.println("Ingresa el coeficiente de x2 en  una funcion objetivo: ");
+    double x2=sol.nextDouble();
+    if (!maximizar) {
+        x1=-x1;
+        x2=-x2;
+    }
+    System.out.println("Ingresa el coeficiente de x1 en restricciones 1 (a11): ");
+    double a11=sol.nextDouble();
+    System.out.println("Ingresa el coeficiente de x2 en restricciones 1 (a12): ");
+    double a12=sol.nextDouble();
+    System.out.println("Ingresa la constaante de restricciones 1 (b1): ");
+    double b1=sol.nextDouble();
+    System.out.println("Ingresa el coeficiente de x1 en restricciones 2 (a21): ");
+    double a21=sol.nextDouble();
+    System.out.println("Ingresa el coeficiente de x2 en restricciones 2 (a22): ");
+    double a22=sol.nextDouble();
+    System.out.println("Ingresa la constante de restricciones 2 (b2): ");
+    double b2=sol.nextDouble();
+    double[][] tabla= new double[3][5];
 }
