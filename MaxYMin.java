f4c86ca22effa7fdfb02e1c3d97d3b685ddc9d49
RoiSol19 <roy.gonzr20@gmail.com>
2025-12-04 22:07:15 -0600

    Menu

---
+++ MaxYMin.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class MaxYMin {
+    public static void main (String[] args){
+        int opcion;
+        Scanner sol=new Sxanner(System.in);
+        do{
+            System.out.println("\n \n \tMenu");
+            System.out.println("1. Maximizar");
+            System.out.println("2. Minimizar");
+            System.out.println("3. Salir");
+            System.out.println("Opcion: ");
+            switch (opcion) {
+                case 1->{
+                    
+                }
+            }
+        }while (opcion!=3);
+    }
+    
+}
